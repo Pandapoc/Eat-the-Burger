ca48961a6aa54c91d452c0ae32e1e89ce25254f4
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-05-20 22:04:33 -0700

    basic routes

--- routes/burgerRoutes.js
+++ routes/burgerRoutes.js
@@ -1,20 +1,24 @@
-const Burger = require('../models')
+const Burger = require('../models/burger.js')
 
 
 module.exports = app => {
     app.get('/', (req, res) => {
         Burger.findAll()
-            .then(burgers => res.render('index', { burgers }))
+            .then(burgers =>
+                res.render('index', { burgers })
+            )
             .catch(e => console.log(e))
     })
 
     app.post('/burger', (req, res) => {
-        Burger.create()
-        res.sendStatus(200)
+        Burger.create(req.body)
+            .then(burgers => { res.render('index', { burgers }) })
+            .catch(e => console.log(e))
     })
 
     app.put('/burger/:id', (req, res) => {
-        Burger.update()
-        res.sendStatus(200)
+        Burger.update(req.body, { where: { id: req.params.id } })
+            .then(burgers => res.render('index', { burgers }))
+            .catch(e => console.log(e))
     })
 }
